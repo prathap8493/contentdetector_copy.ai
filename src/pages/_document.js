d25fe6472e1b5ac49e36a725d47cfbbe3d964473
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-02-10 01:09:23 +0530

    font preloaded

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/nunito/NunitoSans-ExtraBold.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/nunito/NunitoSans-Regular.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
