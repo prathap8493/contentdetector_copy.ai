d23a505e5744448683e295fb5417a2b36c4aea6a
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-02-10 16:31:55 +0530

    url bug fixed

--- src/services/home.js
+++ src/services/home.js
@@ -1,5 +1,5 @@
 export const aiDetectionService = async ({ content }) => {
-  const res = await fetch(`https://cdapi.goom.ai/?${content}`);
+  const res = await fetch(`https://cdapi.goom.ai/?${encodeURI(content)}`);
   const data = await res.json();
   console.log(JSON.stringify(data));
   return data;
