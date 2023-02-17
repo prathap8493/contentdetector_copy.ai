15640c386100a6c13ad99f265175ff74d2e924cb
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-02-17 15:49:28 +0530

    removed react draft css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "@/components/Layout";
 import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
-import "react-quill/dist/quill.snow.css";
 
 export default function App({ Component, pageProps }) {
   return (
