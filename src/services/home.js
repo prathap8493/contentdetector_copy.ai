43346d0989f06a29813f728480b93f4e3364d356
Yashwanth Muddana <77020506+yashh2110@users.noreply.github.com>
2023-03-04 11:28:23 +0530

    endpoint change

--- src/services/home.js
+++ src/services/home.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const aiDetectionService = async ({ content }) => {
   console.log(content);
   const res = await axios.post(
-    `http://3.83.215.200:8080/api/v1/content/detect`,
+    `https://cdapi.goom.ai/api/v1/content/detect`,
 
     {
       content,
